814aa8e99826fd33f59ed1db918fcd101416d448
Puchok-93 <54364298+Puchok-93@users.noreply.github.com>
2024-11-11 18:37:56 +0300

    Подключение стилей, скриптов, добавление меню

---
+++ woo-myminigel/functions.php
@@ -0,0 +1,30 @@
+<?php
+
+// Декларируем поддержку woocommerce
+	function mytheme_add_woocommerce_support() {
+		add_theme_support( 'woocommerce' );
+		add_theme_support( 'title-tag' );
+
+		register_nav_menus( 
+			array(
+				'header-menu' => __('Header Menu'),
+				'footer-menu' => __('Footer Menu'),
+			)
+			);
+	}
+
+    add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+
+// Подключаем стили 
+
+	// function theme_name_scripts() {
+	// 	wp_enqueue_style( 'woocommyshop_style', get_stylesheet_directory_uri() . '/assets/css/style.css' );
+	// }
+
+	add_action( 'wp_enqueue_scripts', function() {
+		wp_enqueue_style( 'woo-myminigel-swiper-style', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css' );
+		wp_enqueue_script( 'woo-myminigel-swiper-script', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', array(), false, true);
+		wp_enqueue_script( 'woo-myminigel-app-script', get_template_directory_uri() . '/assets/js/script.js', array(), false, true);
+	} );
+
+	require_once get_template_directory() . '/incs/woocommerce-hooks.php';
---
+++ woo-myminigel/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ) ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?> >
+
+    <header>
+        <a href="<?php echo home_url( '/' )?>">
+            <img src="<?php echo get_template_directory_uri()?>/assets/img/logo.svg'?>" alt="Myminigel Logo">
+        </a>
+
+
+        <!-- Navigation -->
+        <nav>
+            <?php 
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'header-menu',
+                    )
+                )
+            ?>
+        </nav>
+    </header>
+
+        <?php  wp_body_open(); ?>
